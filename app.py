b93c75c1d1875498ef8bc68363baa6598c42bbb8
dalyl-zero <alidalyl@yahoo.fr>
2020-03-25 19:58:00 +0100

    Add create form with validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from form import ChilliForm
 
 app = Flask(__name__)
@@ -6,14 +6,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\xe8\xaf\xa8\x00\x94^\x1e\x84\\)\xfc\x8d1\xbfN\x8c\x94D&\xdf\x1e\x0f\x1bg\xf0\x98L}2\xb03~'
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return redirect(url_for('create_route'))
+    return redirect(url_for('create'))
 
 
 @app.route('/chilli/variants/create', methods=['GET', 'POST'])
-def create_route():
+def create():
     form = ChilliForm()
+    if form.validate_on_submit():
+        flash(f'{form.name.data}', 'success')
     return render_template('create.html', title='Create', form=form)
 
 
--- form.py
+++ form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, Length
 
 
 class ChilliForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), Length(max=255)])
     scoville = IntegerField('Scoville', validators=[DataRequired(), NumberRange(min=0)])
     days_to_germinate = IntegerField('Days to germinate', validators=[DataRequired(), NumberRange(min=0)])
     create = SubmitField('Create')
